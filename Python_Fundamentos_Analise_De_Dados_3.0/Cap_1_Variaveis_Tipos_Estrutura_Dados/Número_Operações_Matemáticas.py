cde563ccaa7fe2f8b3f7ee38db6b75174df1f7a0
Dev-Nigim <109470518+Dev-Nigim@users.noreply.github.com>
2022-07-27 16:53:51 -0300

    Add Initial

---
+++ Python_Fundamentos_Analise_De_Dados_3.0/Cap_1_Variaveis_Tipos_Estrutura_Dados/Número_Operações_Matemáticas.py
@@ -0,0 +1,10 @@
+a = int(5)  # inteiro
+b = float(3)  # quebrado
+print(type(a))  # comando que mostra o tipo da variavel
+print(f'O resultado da soma é: a+b ', a+b)  # mostra a soma das duas variaveis
+print(f'O resultado da soma é: a+b ', a/b)
+print(f'O resultado da soma é: a+b ', a-b)
+print(f'O resultado da soma é: a+b ', a*b)
+c = float(a)
+print(f"o tipo é", type(c))
+a+b
---
+++ Python_Fundamentos_Analise_De_Dados_3.0/Cap_1_Variaveis_Tipos_Estrutura_Dados/Variáveis_Operadores copy.py
@@ -0,0 +1,7 @@
+teste = 1
+print(teste)
+a, b, c = 'kaio', 'pedro', 'zé'
+a = 'aqui'
+b = 'teste'
+aqui = a+b
+print(aqui)
